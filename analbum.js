1f587d241515c8a0c0df7f4b18d34cd350ef00e6
tmont <tommy.mont@gmail.com>
2024-03-15 00:30:34 -0700

    ignore space/K keypresses if modifier is used

--- analbum.js
+++ analbum.js
@@ -643,6 +643,10 @@
 				switch (e.code) {
 					case 'Space':
 					case 'KeyK':
+						if (modified) {
+							break;
+						}
+
 						e.preventDefault();
 						this.playOrPause();
 						break;
